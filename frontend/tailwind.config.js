544b4673643ab78f2031f7cd6881c9ca20a33612
mitvirani0310 <mit.virani@outamation.com>
2025-01-28 15:45:47 +0530

    chore :  remove the error while running the project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'], // 'content' instead of 'purge'
-  darkMode: 'media', // or 'class'
-  theme: {
-    extend: {
-      colors: {
-        primary: '#3B82F6',
-        secondary: '#64748B',
-      },
-    },
-  },
-  plugins: [
-    import('@tailwindcss/typography'),
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
 
